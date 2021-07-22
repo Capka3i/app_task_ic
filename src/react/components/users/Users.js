873335cae294d1788752d19818f359ec5861ad3b
Capka3i <Capka33i@gmail.com>
2021-07-22 19:23:33 +0300

    Task

--- src/react/components/users/Users.js
+++ src/react/components/users/Users.js
@@ -2,21 +2,24 @@ import {useEffect} from "react";
 import User from "./user/User";
 import {useDispatch, useSelector} from "react-redux";
 import {SET_USERS} from "../../../redux/constsAction";
+import {flex} from "../constElements/styleConst";
+import {URLJSON, USERS} from "../constElements/constElements";
 
 
 function Users(props) {
     const {users} = useSelector((state) => state)
     const dispatch = useDispatch();
-
     const {url} = props.match;
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch(URLJSON+USERS)
             .then(value => value.json())
             .then(value => dispatch({type: SET_USERS, payload: value}))
-    }, {})
+    }, [dispatch])
 
-    return (<div>
+
+
+    return (<div style={flex}>
         {users.map((value, index) => <User key = {index} user = {value} url = {url}/>)}
     </div>);
 }
--- src/react/components/users/user/userDetails/UserDetails.js
+++ src/react/components/users/user/userDetails/UserDetails.js
@@ -1,18 +1,19 @@
 import {Link} from 'react-router-dom';
 import {useSelector} from "react-redux";
+import {buttonStyle, DetailsUser} from "../../../constElements/styleConst";
 
 function UserDetails(props) {
     const {params: {id}} = props.match;
     const {users: {[id]: {name, email, address}}} = useSelector((state) => state);
 
 
-    return (<div>
+    return (<div style = {DetailsUser}>
         {name} <br/>
         {email} <br/>
         {address?.city} <br/>
         st. {address?.street} - {address?.suite} <br/>
         <Link to = {{pathname: '/posts', search: 'userId=' + id}}>
-            <button>Show posts</button>
+            <button style={buttonStyle}>Show posts</button>
         </Link>
     </div>);
 }
