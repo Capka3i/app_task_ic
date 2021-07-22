452c9ddb1391b11e6aa0edb7e0a764bbc1ab54e2
Capka3i <Capka33i@gmail.com>
2021-07-22 19:11:43 +0300

    Task

--- src/react/components/users/Users.js
+++ src/react/components/users/Users.js
@@ -1,23 +1,20 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import User from "./user/User";
 import {useDispatch, useSelector} from "react-redux";
 import {SET_USERS} from "../../../redux/constsAction";
 
 
 function Users(props) {
-    // const someStore = useSelector(state => state)
-    // const dispatch = useDispatch();
-
-    const [users, setUsers] = useState([]);
+    const {users} = useSelector((state) => state)
+    const dispatch = useDispatch();
 
     const {url} = props.match;
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
-            // .then(value => dispatch({type: SET_USERS, payload: value}))
-            .then(value => setUsers([...value]))
-    }, [])
+            .then(value => dispatch({type: SET_USERS, payload: value}))
+    }, {})
 
     return (<div>
         {users.map((value, index) => <User key = {index} user = {value} url = {url}/>)}
